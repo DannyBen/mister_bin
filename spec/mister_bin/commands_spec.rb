6d483ea22f8a8b977fee1a12f20ee2e996550fb7
Danny Ben Shitrit <dannyb@telesofia.com>
2018-05-05 10:23:45 +0000

    pull request updates

--- spec/mister_bin/commands_spec.rb
+++ spec/mister_bin/commands_spec.rb
@@ -26,12 +26,6 @@ describe Commands do
     end
   end
 
-  # describe '#names' do
-  #   it 'returns an array of command names' do
-  #     expect(subject.names).to eq ["ls", "why", "workspace create"]
-  #   end
-  # end
-
   describe '#find' do
     context "when searching for a secondary command" do
       it "returns a matching command" do
