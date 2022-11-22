b88d3f49a2c8917ace207bfa50c28816c2e5b1c5
Danny Ben Shitrit <db@dannyben.com>
2022-11-22 18:50:59 +0000

    - Fix Command#initialize to receive args

--- spec/mister_bin/command_spec.rb
+++ spec/mister_bin/command_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 require_relative '../samples/dir_command'
 
 describe Command do
-  subject(:command) { DirCommand.new }
+  subject(:command) { DirCommand.new args }
+
+  let(:args) { nil }
+
+  describe '#initialize' do
+    let(:args) { { 'FILE' => 'out.txt', '--verbose' => false } }
+
+    it 'accepts hash arguments' do
+      expect(command.args).to eq args
+    end
+  end
 
   describe '#execute' do
     it 'runs the command' do
