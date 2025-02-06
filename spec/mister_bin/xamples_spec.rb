9777a15cfeb170507fefa5e112d9353f29a568cf
Danny Ben Shitrit <db@dannyben.com>
2025-02-06 10:22:11 +0000

    - Drop support for Ruby 3.0

--- spec/mister_bin/xamples_spec.rb
+++ spec/mister_bin/xamples_spec.rb
@@ -1,6 +1,7 @@
 describe 'Examples' do
   Dir['examples/*'].select { |f| File.directory? f }.each do |example|
     name = File.basename(example)
+    leeway = RUBY_VERSION < '3.4.0' ? 10 : 0
 
     describe name do
       lines = Dir.chdir(example) { File.readlines('app.rb').filter_map { |line| line[/^# \$ (.*)/, 1] } }
@@ -11,7 +12,7 @@ describe 'Examples' do
 
         it "executes #{line}" do
           Dir.chdir(example) do
-            expect(`#{line}`).to match_approval(fixture_name)
+            expect(`#{line}`).to match_approval(fixture_name).diff(leeway)
           end
         end
       end
